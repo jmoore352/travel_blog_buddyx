de08e39d598950f164ccaf88429b97cebef648a3
Justin Moore <jmoore3@chicagobooth.edu>
2015-12-09 02:06:08 -0600

    add in admin location rb

--- app/admin/location.rb
+++ app/admin/location.rb
@@ -4,6 +4,15 @@ ActiveAdmin.register Location do
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
  permit_params :category, :our_review, :destination_summary, :our_rating, :price, :city_country, :date, :image
+
+  form do |f|
+    f.inputs 'Details' do
+      f.input :city_country, :as => :string
+    end
+    f.actions
+  end
+
+
 #
 # or
 #
